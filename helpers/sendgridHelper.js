9508f6dc2ebc431fcce739927da96deff1df9074
Ryan Griego <ryangriego@gmail.com>
2023-12-19 12:14:05 -0800

    added env

--- helpers/sendgridHelper.js
+++ helpers/sendgridHelper.js
@@ -1,6 +1,6 @@
 const client = require('@sendgrid/mail');
 
-client.setApiKey("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");
+client.setApiKey("");
 
 const sendMessage = async (message) => {
   let response = null;
