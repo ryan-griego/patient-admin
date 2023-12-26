87ad105703c9014cb67007efd71aa26f61d2b3c7
Ryan Griego <ryangriego@gmail.com>
2023-12-26 14:59:33 -0800

    updated table header values and modified existing patient weight loss email

--- app.vue
+++ app.vue
@@ -1,113 +1,121 @@
 <template>
-   <div id="app">
-      <button @click="SendEmail">fsfsfdsf</button>
-    </div>
+    <v-container>
+      <v-row align="center"
+        justify="center">
+        <v-col
+          class="mb-5"
+          cols="12"
+        >
+    <v-data-table
+      :headers="headers"
+      :items="patients"
+      :sort-by="[{ key: 'name', order: 'asc' }]"
 
-
-  <v-data-table
-    :headers="headers"
-    :items="patients"
-    :sort-by="[{ key: 'calories', order: 'asc' }]"
-  >
-    <template v-slot:top>
-      <v-toolbar flat>
-        <v-toolbar-title>Patient Admin</v-toolbar-title>
-        <v-divider class="mx-4" inset vertical></v-divider>
-        <v-spacer></v-spacer>
-        <v-dialog v-model="dialog" max-width="500px">
-          <template v-slot:activator="{ props }">
-            <v-btn color="primary" dark class="mb-2" v-bind="props">
-              New Item
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="text-h5">{{ formTitle }}</span>
-            </v-card-title>
-
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.name"
-                      label="Patient name"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.calories"
-                      label="Name"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.fat"
-                      label="Fat (g)"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.carbs"
-                      label="Carbs (g)"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.protein"
-                      label="Protein (g)"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue-darken-1" variant="text" @click="close">
-                Cancel
+    >
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Patient Admin</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+          <v-spacer></v-spacer>
+          <v-dialog v-model="dialog" max-width="500px">
+            <template v-slot:activator="{ props }">
+              <v-btn color="primary" dark class="mb-2" v-bind="props">
+                New Patient
               </v-btn>
-              <v-btn color="blue-darken-1" variant="text" @click="save">
-                Save
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="500px">
-          <v-card>
-            <v-card-title class="text-h5"
-              >Are you sure you want to delete this item?</v-card-title
-            >
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue-darken-1" variant="text" @click="closeDelete"
-                >Cancel</v-btn
-              >
-              <v-btn
-                color="blue-darken-1"
-                variant="text"
-                @click="deleteItemConfirm"
-                >OK</v-btn
-              >
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="text-h5">{{ formTitle }}</span>
+              </v-card-title>
 
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-      </v-toolbar>
-    </template>
-    <template v-slot:item.actions="{ item }">
-      <v-icon size="small" class="me-2" @click="editItem(item)">
-        mdi-pencil
-      </v-icon>
-      <v-icon size="small" @click="deleteItem(item)"> mdi-delete </v-icon>
-      <v-icon size="small" @click="SendEmail(item)"> mdi-mail </v-icon>
-    </template>
-    <template v-slot:no-data>
-      <v-btn color="primary" @click="initialize"> Reset </v-btn>
-    </template>
-  </v-data-table>
-</template>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.name"
+                        label="Patient name"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.calories"
+                        label="Email"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.fat"
+                        label="Medication"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.carbs"
+                        label="Gender"
+                      ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="4">
+                      <v-text-field
+                        v-model="editedItem.condition"
+                        label="Condition"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue-darken-1" variant="text" @click="close">
+                  Cancel
+                </v-btn>
+                <v-btn color="blue-darken-1" variant="text" @click="save">
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog v-model="dialogDelete" max-width="600px">
+            <v-card>
+              <v-card-title class="text-h5"
+                >Are you sure you want to delete this patient?</v-card-title
+              >
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue-darken-1" variant="text" @click="closeDelete"
+                  >Cancel</v-btn
+                >
+                <v-btn
+                  color="blue-darken-1"
+                  variant="text"
+                  @click="deleteItemConfirm"
+                  >OK</v-btn
+                >
+
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template v-slot:item.actions="{ item }">
+        <v-icon size="small" class="me-2" @click="editItem(item)">
+          mdi-pencil
+        </v-icon>
+        <v-icon size="small" @click="deleteItem(item)"> mdi-delete </v-icon>
+        <v-icon size="small" @click="SendEmail(item)"> mdi-mail </v-icon>
+      </template>
+      <template v-slot:no-data>
+        <v-btn color="primary" @click="initialize"> Reset </v-btn>
+      </template>
+    </v-data-table>
+
+     </v-col>
+    </v-row>
+  </v-container>
+
+ </template>
 
 <script>
 
@@ -126,7 +134,7 @@
         { title: 'Email', key: 'email' },
         { title: 'Medication', key: 'medication' },
         { title: 'Gender', key: 'gender' },
-        { title: 'isVip', key: 'isvip' },
+        { title: 'Condition', key: 'condition' },
         { title: 'Actions', key: 'actions', sortable: false },
       ],
       patients: [],
@@ -136,14 +144,14 @@
         email: '',
         medication: '',
         gender: '',
-        isvip: true,
+        condition: 'Diabetes',
       },
       defaultItem: {
         name: '',
         email: '',
         medication: '',
         gender: '',
-        isvip: true,
+        condition: 'Diabetes',
       },
        msg: {
       "personalizations": [
@@ -198,25 +206,25 @@
       initialize() {
         this.patients = [
           {
-            name: 'George',
+            name: 'Johnny',
             email: 'ryangriego@gmail.com',
-            medication: '',
+            medication: 'Ozempic',
             gender: 'male',
-            isvip: true,
+            condition: 'Diabetes',
           },
           {
-            name: 'Pam',
+            name: 'Lisa',
             email: 'ryangriego@gmail.com',
-            medication: '',
+            medication: 'Ozempic',
             gender: 'female',
-            isvip: true,
+            condition: 'Diabetes',
           },
           {
             name: 'Mark',
             email: 'ryangriego@gmail.com',
-            medication: '',
+            medication: 'Ozempic',
             gender: 'male',
-            isvip: true,
+            condition: 'Diabetes',
           },
         ]
       },
@@ -254,11 +262,16 @@
       let name =  item.name;
       let medication = item.medication;
       let gender = item.gender;
-      let trackingUrl = 'test';
+      console.log("log the gender", gender);
+      let heroImage = '';
 
-      let position = 'j';
-      let company = 'l';
-
+      // this can be improved by passing in the gender value into the url and each image either ends with male or female
+      if(gender == 'male') {
+        console.log('got in male');
+        heroImage = 'src="https://res.cloudinary.com/dm7y3yvjp/image/upload/v1703108432/lifemd-logo_ecac59.png"';
+      } else {
+        heroImage = 'src="https://res.cloudinary.com/dm7y3yvjp/image/upload/v1703108811/doctors_xlifii.png"';
+      }
 
       let msg = {
         "personalizations": [
@@ -275,11 +288,11 @@
           email: "ryangriego@gmail.com",
           name: "Ryan Griego / Web Developer"
         },
-        "subject": `Recently applicant for ${position} at ${company} - a few resources to get to know me`,
+        "subject": `Recently applicant for ${medication} at ${gender} - a few resources to get to know me`,
         "content": [
           {
             type: "text/plain",
-            value: `Recently applicant for ${position} at ${company} - a few resources to get to know me`
+            value: `Recently applicant for ${medication} at ${gender} - a few resources to get to know me`
           },
           {
             type: "text/html",
@@ -542,7 +555,7 @@
                         </div>
                         <div class="mj-inline-links" style="">
                           <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0" align="center"><tr><td style="padding:15px 10px;" class="" ><![endif]-->
-                          <a class="mj-link" href="https://www.ryangriego.comhttps://www.ryangriego.com/"
+                          <a class="mj-link" href="https://www.ryangriego.com/"
                             target="_blank"
                             style="display:inline-block;color:#000000;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:13px;font-weight:normal;line-height:22px;text-decoration:none;text-transform:uppercase;padding:15px 10px;">
                             <span style="letter-spacing:.2px">Member Portal</span>
@@ -586,11 +599,10 @@
                         style="font-size:0px;padding:10px 25px;padding-top:40px;padding-right:25px;padding-bottom:10px;padding-left:25px;word-break:break-word;">
                         <div
                           style="font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:40px;line-height:1;text-align:left;color:#ffffff;">
-                          <p style="font-size: 22px;line-height: 1.8rem">Hi ${name},</p>
-                          <p style="font-size: 13px;line-height: 1.2rem;">I heard ${company} is looking for a
-                            ${position}.<br /></p>
-                          <p style="font-size: 13px;line-height: 1.2rem;">I applied for the position yesterday and
-                            wanted to reach out and further express my interest in the&nbsp;position.<br /></p>
+                          <p style="font-size: 22px;line-height: 1.8rem">Dear ${name},</p>
+                          <p style="font-size: 13px;line-height: 1.2rem;">
+Your commitment to weight loss is commendable and showcases your inner strength and resilience. Each healthy choice is a step toward a brighter, more vibrant future.
+Wishing you continued success and good health,<br /></p>
                           <!-- <p>Try Me</p>
           <p>FREE</p> -->
                         </div>
@@ -607,7 +619,7 @@
                               valign="middle">
                               <a href="https://www.ryangriego.com/"
                                 style="display:inline-block;background:#ffffff;color:#1a9ba2;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:16px;font-weight:normal;line-height:120%;margin:0;text-decoration:none;text-transform:none;padding:10px 25px;mso-padding-alt:0px;border-radius:3px;"
-                                target="_blank"> Check Me out </a>
+                                target="_blank"> View Profile </a>
                             </td>
                           </tr>
                         </table>
@@ -630,9 +642,9 @@
                           <tbody>
                             <tr>
                               <td style="width:400px;">
-                                <img alt="Clothes set" height="auto"
-                                  src="https://res.cloudinary.com/dm7y3yvjp/image/upload/v1703108811/doctors_xlifii.png"
-                                  style="border:none;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;"
+                                <img alt="Doctors" height="auto"
+                                  ${heroImage}
+                                  style="border:none;display:block;outline:none;text-decoration:none;height:auto;width:75%;font-size:13px;"
                                   width="400" />
                               </td>
                             </tr>
@@ -788,7 +800,7 @@ Call me maybe?
     </div>
     <!--[if mso | IE]></td></tr></table><![endif]-->
   </div>
-  <img src=${trackingUrl}>
+
 </body>
 </html>
 
